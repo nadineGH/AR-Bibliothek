c3097f74092c726795473fc1a75bc95b0123af8b
NADINE <n.winter93@gmx.de>
2017-06-05 17:25:42 +0200

    Pilz Model hinzugefügt

---
+++ AR Buecherhalle/Assets/ButtonActions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonActions : MonoBehaviour {
+    public Animator pilzAnim;
+    // Use this for initialization
+    
+	
+	// Update is called once per frame
+	void Update () {
+        pilzAnim.Play("Windig");
+    }
+}
--- AR Buecherhalle/Assets/animation.cs
+++ AR Buecherhalle/Assets/animation.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class animation : MonoBehaviour {
+    public Animator pilzAnim;
+    // Use this for initialization
+    void Start () {
 
-	// Use this for initialization
-	void Start () {
-		
+        pilzAnim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (Input.GetKeyDown("q"))
+        {
+            pilzAnim.Play("Windig");
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            pilzAnim.Play("ArmatureAction");
+        }
+    }
 }
