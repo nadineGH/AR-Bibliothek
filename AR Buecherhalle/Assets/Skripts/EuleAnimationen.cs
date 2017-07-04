2c609e9d66306e8f652a7f246d30b11270fb8d23
NADINE <n.winter93@gmx.de>
2017-07-04 23:49:32 +0200

    Animationen und neues Tracking Bild

---
+++ AR Buecherhalle/Assets/Skripts/EuleAnimationen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EuleAnimationen : MonoBehaviour {
+
+    public Animator eule;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        eule = GetComponent<Animator>();
+    }
+
+    
+    void Bewegung()
+    {
+       
+       // eule.Play("Armature|KopfRechtsSchief");
+        /*StartCoroutine(MyMethod());
+        eule.Play("Armature|KopfLinksSchief");
+        StartCoroutine(MyMethod());
+        eule.Play("Armature|Nicken");
+        StartCoroutine(MyMethod());
+        eule.Play("Armature|kopfDrehenL");
+        StartCoroutine(MyMethod());
+        eule.Play("Armature|kopfDrehenR");
+        StartCoroutine(MyMethod());
+        eule.Play("Armature|KoerperDrehen");*/
+        
+    }
+
+    IEnumerator MyMethod()
+    {
+        
+        yield return new WaitForSeconds(2);
+        
+    }
+}
--- AR Buecherhalle/Assets/animation.cs
+++ AR Buecherhalle/Assets/animation.cs
@@ -3,23 +3,69 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class animation : MonoBehaviour {
-    public Animator pilzAnim;
+    public Animator eule;
+    int num = 1;
     // Use this for initialization
     void Start () {
 
-        pilzAnim = GetComponent<Animator>();
+        eule = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKeyDown("q"))
-        {
-            pilzAnim.Play("Windig");
-        }
 
-        if (Input.GetKeyDown("w"))
-        {
-            pilzAnim.Play("ArmatureAction");
-        }
+    public void BewegungA()
+    {
+        
+       eule.Play("Armature|Nicken");
+        
     }
+    public void BewegungB()
+    {
+       eule.Play("Armature|KoerperDrehen");
+    }
+    public void BewegungC()
+    {
+        eule.Play("Armature|kopfDrehenR");
+    }
+    public void BewegungD()
+    {
+        eule.Play("Armature|kopfDrehenL 1");
+    }
+    public void BewegungE()
+    {
+        eule.Play("Armature|KopfLinksSchief");
+    }
+    public void BewegungF()
+    {
+        eule.Play("Armature|KopfRechtsSchief");
+    }
+    /*if (num == 2)
+    {
+        eule.Play("Armature|Nicken");
+        num ++;
+    }
+    if (num == 3)
+    {
+        eule.Play("Armature|kopfDrehenL 1");
+    }
+    */
+
+    //eule.Play("Armature|KopfRechtsSchief");
+    //StartCoroutine(MyMethod());
+    //eule.Play("Armature|KopfLinksSchief");
+    //StartCoroutine(MyMethod());
+
+    // StartCoroutine(MyMethod());
+    //eule.Play("Armature|kopfDrehenL 1");
+    //StartCoroutine(MyMethod());
+    // eule.Play("Armature|kopfDrehenR");
+    //StartCoroutine(MyMethod());
+    //eule.Play("Armature|KoerperDrehen");
+
 }
+
+    /*IEnumerator MyMethod()
+    {
+
+        yield return new WaitForSeconds(1);
+
+    }*/
+
