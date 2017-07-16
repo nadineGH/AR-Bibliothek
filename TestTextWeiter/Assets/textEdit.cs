f370f5ed4895ee3ef29565a5d20171f069a3fd11
PRGAnna <mdina@gmx.net>
2017-07-16 21:34:03 +0200

    Testprojekt Unity erstellt

---
+++ TestTextWeiter/Assets/textEdit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	[SerializeField]
+	private Text text = null;
+
+
+	// Use this for initialization
+	void Start () {
+		text.text = "Hallo Mensch";
+	}
+
+}
