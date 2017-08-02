99b949f990896f814c25d3eeb1b2b4113e8bec58
NADINE <n.winter93@gmx.de>
2017-08-02 22:06:19 +0200

    Pilze sammeln Funktion erstellt

---
+++ AR Buecherhalle/Assets/Zaehler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zaehler : MonoBehaviour
+{
+    //[SerializeField]
+    public Text anzahlText;
+    public GameObject pilz;
+    public GameObject pilz2;
+    // Use this for initialization
+    /*void Start () {
+        anzahl = 0;
+        UpdateAnzahl();
+	}*/
+
+    // Update is called once per frame
+   public void UpdateAnzahl()
+    {
+        if (pilz.activeInHierarchy == false | pilz2.activeInHierarchy == false)
+        {
+            
+            StatischeVariable.pilzAnzahl++;
+            anzahlText.text = "Pilze: " + StatischeVariable.pilzAnzahl;
+            Debug.Log(StatischeVariable.pilzAnzahl + "Pilze");
+        }
+    }
+}
