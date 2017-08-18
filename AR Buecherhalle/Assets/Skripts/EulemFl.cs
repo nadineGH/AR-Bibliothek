556c75fba188afa80bee4fdc945660187791b99e
NADINE <n.winter93@gmx.de>
2017-08-18 21:56:24 +0200

    Bewegungen der Eule bearbeitet

---
+++ AR Buecherhalle/Assets/Skripts/EulemFl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EulemFl : MonoBehaviour {
+
+    public Animator eule;
+    
+    void Start () {
+
+        eule = GetComponent<Animator>();
+        //StartCoroutine(MyMethod());
+        //eule.Play("Armature|Flügel");
+        //StartCoroutine(MyMethod());
+        //eule.Play("Armature|Nicken");
+
+    }
+
+    void Update ()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+
+            eule.Play("Armature|Nicken");
+
+        }
+    }
+    IEnumerator MyMethod()
+    {
+
+       yield return new WaitForSeconds(1);
+
+
+    }
+
+    
+}
