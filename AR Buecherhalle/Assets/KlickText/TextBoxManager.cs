3b1c7dca47c9fae2e6f0c5c3637c3a488a12e0c3
NADINE <n.winter93@gmx.de>
2017-08-31 15:06:20 +0200

    Text Interaktionen bearbeitet

--- AR Buecherhalle/Assets/KlickText/TextBoxManager.cs
+++ AR Buecherhalle/Assets/KlickText/TextBoxManager.cs
@@ -7,15 +7,21 @@ public class TextBoxManager : MonoBehaviour {
 
     public GameObject textBox;
     public Button weiterButton;
-	public Text theText;
+    public Button jaButton;
+    public Button neinButton;
+    public Text theText;
 
-	public TextAsset textFile;
-	public string[] textLines;
+    public TextAsset textFile;
+    public string[] textLines;
 
-	public int currentLine;
-	public int endAtLine;
-	
-	void Start () {
+    public int currentLine;
+    public int endAtLine;
+
+    public GameObject demoPilz;
+    public GameObject pilz;
+
+    void Start()
+    {
 
         if (textFile != null)
         {
@@ -27,9 +33,10 @@ public class TextBoxManager : MonoBehaviour {
             endAtLine = textLines.Length - 1;
         }
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
 
         theText.text = textLines[currentLine];
 
@@ -37,19 +44,61 @@ public class TextBoxManager : MonoBehaviour {
         {
             currentLine += 1;
         }
+
+        // Ein Pilz wurde gesammelt -> Glückwunsch Text
+        if (StatischeVariable.pilzAnzahl == 1)
+        {
+            StatischeVariable.pilzAnzahl++;
+            textBox.SetActive(true);
+            currentLine = 16;
+            StartCoroutine(MyMethod());
+        }
+    }
+
+    IEnumerator MyMethod()
+    {
+        yield return new WaitForSeconds(5);
+        textBox.SetActive(false);
+        yield break;
     }
 
     public void Weiter()
     {
+        weiterButton.gameObject.SetActive(true);
         theText.text = textLines[currentLine];
         currentLine += 1;
-        if (currentLine == 10)
+        //Demo Pilz verschwinden lassen
+        /* if (currentLine == 4)
+         {
+             demoPilz.SetActive(false);
+             pilz.SetActive(true);
+             textBox.SetActive(false);
+             weiterButton.gameObject.SetActive(false);
+         }*/
+        if (currentLine == 11)
         {
-            textBox.SetActive(false);
+            jaButton.gameObject.SetActive(true);
+            neinButton.gameObject.SetActive(true);
+            //textBox.SetActive(false);
             weiterButton.gameObject.SetActive(false);
 
         }
+        if (currentLine == 15)
+        {
+            demoPilz.SetActive(false);
+            pilz.SetActive(true);
+            textBox.SetActive(false);
+            weiterButton.gameObject.SetActive(false);
+        }
+
     }
+    public void Ja()
+    {
+        neinButton.gameObject.SetActive(false);
+        theText.text = textLines[currentLine];
+        currentLine += 1;
+        Weiter();
+        
 
+    }
 }
-
